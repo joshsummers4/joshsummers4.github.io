810873dfe0b479b823c446d310112168cc236d8e
joshsummers4 <67658350+joshsummers4@users.noreply.github.com>
2022-01-21 16:17:11 +0700

    about me section and background fix all browsers

--- functions/canvas.js
+++ functions/canvas.js
@@ -1,13 +1,19 @@
-function draw(w, h) {
-    var ctx = document.getCSSCanvasContext("2d", "lines", w, h);
-    ctx.beginPath();
-    ctx.moveTo(25, 0);
-    ctx.lineTo(0, 25);
-    ctx.lineWidth = 2;
-    // set line color
-    ctx.strokeStyle = 'rgba(251, 255, 254, 0.5';
-    ctx.stroke();
+function draw(){
+    var c = document.createElement('canvas'),        
+    ctx = c.getContext('2d'),
+    cw = c.width = 100;
+    c.height = 100;
+    ctx.lineWidth = 1;
+    for(var x = 0; x < cw; x++){
+        ctx.beginPath();
+        ctx.moveTo(25, 0);
+        ctx.lineTo(0, 25);
+        ctx.strokeStyle = 'rgb(109, 103, 110)';
+        ctx.stroke();
+    }
+    document.body.style.background = 'url(' + c.toDataURL() + ')';
+    document.body.style.backgroundColor = 'rgb(27, 27, 30)';
+    document.body.style.backgroundAttachment = 'fixed';
 }
-
-
+        
 
